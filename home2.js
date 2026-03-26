96ca2a3e349413d0195d80fa34152a60c18b2552
unknown <jishnu4211@gmail.com>
2026-03-26 13:01:21 +0530

    loop2

---
+++ home2.js
@@ -0,0 +1,23 @@
+// BEGINNER
+//Exercise 1: Sum All Numbers
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum); 
+//Exercise 2: Print Array 
+// let animals = ["cat", "dog", "bird"];
+
+// for (let animal of animals) {
+//   console.log(animal);
+// }
+// //Exercise 3: Object Properties
+// let person = { name: "Alice", age: 25, city: "NYC" };
+
+// for (let key in person) {
+//   console.log(key + ": " + person[key]);
+// }
+
